791fd70aa78b42cfcee9f39dd75050f5910e18b5
ValkovStoil <valkovstoil@abv.bg>
2020-03-08 17:15:39 +0200

    Reverse String with Stack

---
+++ Reverse String/Reverse String/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Reverse_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var inputStack = new Stack<char>(input);
+
+            while (inputStack.Count != 0)
+            {
+                Console.Write(inputStack.Pop());
+
+            }
+
+        }
+    }
+}

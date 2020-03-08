1db115b2a8aed8f0642511e7f3830b29ffa5012b
ValkovStoil <valkovstoil@abv.bg>
2020-03-08 18:19:45 +0200

    Simple Calculator

---
+++ Simple Calculator/Simple Calculator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Simple_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split().Reverse();
+            var stackInput = new Stack<string>(input);
+            stackInput.Reverse();
+            var sum = int.Parse(stackInput.Pop());
+
+            while (stackInput.Count != 0)
+            {
+                var action = stackInput.Pop();
+
+                switch (action)
+                {
+
+                    case "-": 
+                        sum -= int.Parse(stackInput.Pop());
+                        break;
+                    case "+":
+                        sum += int.Parse(stackInput.Pop());
+                        break;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}

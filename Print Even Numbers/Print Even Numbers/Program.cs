78b61b97fa324cd4d1f6797fd2e3d894c7c75731
ValkovStoil <valkovstoil@abv.bg>
2020-03-09 16:12:33 +0200

    Print Even Numbers

---
+++ Print Even Numbers/Print Even Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Print_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split().Select(n => int.Parse(n));
+            var que = new Queue<int>(input);
+            var output = new List<int>();
+
+            while (que.Count != 0)
+            {
+                var numToChek = que.Dequeue();
+                
+                if(numToChek % 2 == 0)
+                {
+                    output.Add(numToChek);
+                }
+            }
+            Console.Write(String.Join(", ", output.ToArray()));
+        }
+    }
+}

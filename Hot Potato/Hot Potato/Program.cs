19ab006a9f03a4cc534cc0b34b75cc55779598d7
ValkovStoil <valkovstoil@abv.bg>
2020-03-09 16:58:11 +0200

    Hot Potato

---
+++ Hot Potato/Hot Potato/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hot_Potato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(" ");
+            var num = int.Parse(Console.ReadLine());
+
+            var children = new Queue<string>(input);
+
+            while (children.Count > 1)
+            {
+                for (int i = 0; i < num - 1; i++)
+                {
+                    var name = children.Dequeue();
+                    children.Enqueue(name);
+                }
+
+                Console.WriteLine($"Removed {children.Dequeue()}");
+            }
+            Console.WriteLine($"Last is {children.Dequeue()}");
+        }
+    }
+}

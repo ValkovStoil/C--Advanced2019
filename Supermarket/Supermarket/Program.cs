7b3089a32b16da4f4f23d3120bdf1f9405eebdd2
ValkovStoil <valkovstoil@abv.bg>
2020-03-09 16:24:40 +0200

    Supermarket

---
+++ Supermarket/Supermarket/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputName = Console.ReadLine();
+            var queNames = new Queue<string>();
+
+            while (inputName != "End")
+            {
+                if (inputName == "Paid")
+                {
+                    while (queNames.Count != 0)
+                    {
+                        Console.WriteLine($"{queNames.Dequeue()}");
+                    }
+                }
+                else
+                { 
+                   queNames.Enqueue(inputName);
+                }
+                inputName = Console.ReadLine();
+            }
+            Console.WriteLine($"{queNames.Count()} people remaining.");
+        }
+    }
+}

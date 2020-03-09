f1e952b3198b99278cc9657a3f6b1f349d163f89
ValkovStoil <valkovstoil@abv.bg>
2020-03-09 16:10:54 +0200

    Matching Brackets

---
+++ Matching Brackets/Matching Brackets/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Matching_Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var helpStack = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if(input[i] == '(')
+                {
+                    helpStack.Push(i);
+                }
+                else if (input[i] == ')')
+                {
+                    var index = helpStack.Pop();
+                    Console.WriteLine(input.Substring(index, i - index + 1));
+                }
+            }
+        }
+    }
+}

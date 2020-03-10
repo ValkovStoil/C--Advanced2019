f1588e7fda130d8c698cefe761bb22ba7faf47c0
ValkovStoil <valkovstoil@abv.bg>
2020-03-10 17:22:58 +0200

    Primary Diagonal

---
+++ Primary Diagonal/Primary Diagonal/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace Primary_Diagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = int.Parse(Console.ReadLine());
+
+            var matrix = new int[num, num];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var inputMatrixNumbers = Console.ReadLine().Split(" ")
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = inputMatrixNumbers[col];
+                }
+            }
+
+            var sum = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                sum += matrix[row, row];
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}

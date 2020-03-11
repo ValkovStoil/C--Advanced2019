64341cf1485b095545b91d6c7fa642a9552b2e7a
ValkovStoil <valkovstoil@abv.bg>
2020-03-11 17:47:30 +0200

    Count Same Values in Array

---
+++ Count Same Values in Array/Count Same Values in Array/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Count_Same_Values_in_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(" ")
+                .Select(double.Parse)
+                .ToArray();
+
+            var dict = new Dictionary<double, int>();
+
+
+            foreach (var num in input)
+            {
+                if (!dict.ContainsKey(num))
+                {
+                    dict[num] = 0;
+                }
+                dict[num]++;
+            }
+
+            foreach (var kvp in dict)
+            {
+                Console.WriteLine($"{kvp.Key} - {kvp.Value} times");
+            }
+        }
+    }
+}

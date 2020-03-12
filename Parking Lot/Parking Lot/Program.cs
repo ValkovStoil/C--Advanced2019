d5c09ed0d6d06bc03e0f1914441cf508cce4a481
ValkovStoil <valkovstoil@abv.bg>
2020-03-12 15:05:27 +0200

    Parking Lot

---
+++ Parking Lot/Parking Lot/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Parking_Lot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var carNumber = Console.ReadLine().Split(", ");
+            var carsIn = new HashSet<string>();
+
+            while (carNumber[0] != "END")
+            {
+                var direction = carNumber[0];
+                var number = carNumber[1];
+
+                switch (direction)
+                {
+                    case "IN":
+                        carsIn.Add(number);
+                        break;
+                    case "OUT":
+                        carsIn.Remove(number);
+                        break;
+                }
+
+                carNumber = Console.ReadLine().Split(", ");
+            }
+
+            if (carsIn.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            else
+            {
+                foreach (var car in carsIn)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+        }
+    }
+}

7d5371a54240786f9bf938c1308d1241e81a7546
ValkovStoil <valkovstoil@abv.bg>
2020-03-12 14:50:26 +0200

    Record Unique Names

---
+++ Record Unique Names/Record Unique Names/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Record_Unique_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var unique = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var name = Console.ReadLine();
+
+                unique.Add(name);
+            }
+
+            foreach (var name in unique)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}

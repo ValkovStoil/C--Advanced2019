4d34e60e847e1570c12f27dfc30fc4c006e3e6d3
ValkovStoil <valkovstoil@abv.bg>
2020-03-13 17:55:50 +0200

    Fashion Boutique

---
+++ Fashion Boutique/Fashion Boutique/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fashion_Boutique
+{
+    class Program
+    {
+        /*
+         * 5 4 8 6 3 8 7 7 9
+16
+         */
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            var racksStorage = int.Parse(Console.ReadLine());
+            var cloths = new Stack<int>(input);
+            var countOfRacks = 1;
+            var sumOfCloths = 0;
+
+            while (cloths.Count > 0)
+            {
+                sumOfCloths += cloths.Peek();
+                if(sumOfCloths <= racksStorage)
+                {
+                    cloths.Pop();
+                }
+                else
+                {
+                    countOfRacks++;
+                    sumOfCloths = 0;
+                }
+            }
+
+            Console.WriteLine(countOfRacks);
+        }
+    }
+}

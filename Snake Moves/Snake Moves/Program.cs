39eda1a01daabed546df0945578f1b8e4d43fa26
ValkovStoil <valkovstoil@abv.bg>
2020-03-19 17:46:17 +0200

    TODO Snake Moves

---
+++ Snake Moves/Snake Moves/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Snake_Moves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dimensions = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            var snake = Console.ReadLine().Trim();
+
+            var rows = dimensions[0];
+            var columns = dimensions[1];
+
+            var matrix = new string[rows, columns];
+
+            //TODO
+        }
+    }
+}

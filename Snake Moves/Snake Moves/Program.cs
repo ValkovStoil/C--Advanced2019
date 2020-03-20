240e6ea5c9604ae4b5a44f7f90aa959e45fbe9a5
ValkovStoil <valkovstoil@abv.bg>
2020-03-20 14:54:46 +0200

    Snake Moves Done

--- Snake Moves/Snake Moves/Program.cs
+++ Snake Moves/Snake Moves/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Snake_Moves
@@ -13,12 +14,46 @@ namespace Snake_Moves
                 .ToArray();
             var snake = Console.ReadLine().Trim();
 
+            var snakeQueue = new Queue<char>(snake.ToCharArray());
+
             var rows = dimensions[0];
             var columns = dimensions[1];
 
-            var matrix = new string[rows, columns];
+            var matrix = new char[rows, columns];
 
             //TODO
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                if (row % 2 == 0)
+                {
+
+                    for (int col = 0; col < matrix.GetLength(1); col++)
+                    {
+                        var element = snakeQueue.Dequeue();
+                        matrix[row, col] = element;
+                        snakeQueue.Enqueue(element);
+                    }
+                }
+                else
+                {
+                    for (int col = matrix.GetLength(1) - 1; col >= 0; col--)
+                    {
+
+                        var element = snakeQueue.Dequeue();
+                        matrix[row, col] = element;
+                        snakeQueue.Enqueue(element);
+                    }
+                }
+            }
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }

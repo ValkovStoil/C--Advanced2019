0b3379b8b8072dc5f58961538177e247a46784d1
ValkovStoil <valkovstoil@abv.bg>
2020-03-26 16:10:40 +0200

    Sets of Elements

---
+++ Sets of Elements/Sets of Elements/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sets_of_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var n = input[0];
+            var m = input[1];
+
+
+            var first = new HashSet<int>();
+            var second = new HashSet<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                first.Add(int.Parse(Console.ReadLine()));
+            }
+            for (int i = 0; i < m; i++)
+            {
+                second.Add(int.Parse(Console.ReadLine()));
+            }
+
+            Console.Write(string.Join(" ", first.Intersect(second)));
+        }
+    }
+}

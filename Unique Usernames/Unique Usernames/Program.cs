8b729f390f672a060ce7d639dc99b01c63aa9596
ValkovStoil <valkovstoil@abv.bg>
2020-03-26 15:13:04 +0200

    Unique Usernames

---
+++ Unique Usernames/Unique Usernames/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Unique_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var collection = new HashSet<string>();
+
+            var userNamesCount = int.Parse(Console.ReadLine());
+
+            for (int user = 0; user < userNamesCount; user++)
+            {
+                var userName = Console.ReadLine();
+
+                collection.Add(userName);
+            }
+
+            foreach (var name in collection)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}

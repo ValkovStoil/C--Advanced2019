a7e66f986acdd0843641b85823016546fff47e92
ValkovStoil <valkovstoil@abv.bg>
2020-03-26 16:38:56 +0200

    Even Times

---
+++ Even Times/Even Times/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Even_Times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new Dictionary<int, int>();
+
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var num = int.Parse(Console.ReadLine());
+
+                if (!numbers.ContainsKey(num))
+                {
+                    numbers[num] = 0;
+                }
+                numbers[num]++;
+            }
+
+            numbers = numbers.Where(x => x.Value % 2 == 0)
+                .ToDictionary(k => k.Key, v => v.Value);
+
+            foreach (var num in numbers)
+            {
+                Console.WriteLine(num.Key);
+            }
+        }
+    }
+}

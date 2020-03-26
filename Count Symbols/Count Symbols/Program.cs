f41bfc96fac83102769ea7b7e18fca6847a56724
ValkovStoil <valkovstoil@abv.bg>
2020-03-26 16:45:41 +0200

    Count Symbols

---
+++ Count Symbols/Count Symbols/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Count_Symbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var symbols = new SortedDictionary<char, int>();
+
+            var stringInput = Console.ReadLine();
+
+            foreach (var @char in stringInput)
+            {
+                if (!symbols.ContainsKey(@char))
+                {
+                    symbols[@char] = 0;
+                }
+                symbols[@char]++;
+            }
+
+            foreach (var kvp in symbols)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value} time/s");
+            }
+        }
+    }
+}

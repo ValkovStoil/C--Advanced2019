1b67749bb819b1739af29833aa2974dcb7abcdcb
ValkovStoil <valkovstoil@abv.bg>
2020-03-26 16:32:41 +0200

    Periodic Table

---
+++ Periodic Table/Periodic Table/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var elements = new SortedSet<string>();
+
+            var n = int.Parse(Console.ReadLine());
+
+            for (int el = 0; el < n; el++)
+            {
+                var chemicals = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                foreach (var ch in chemicals)
+                {
+                    elements.Add(ch);
+                }
+            }
+
+            Console.Write(string.Join(" ", elements));
+
+        }
+    }
+
+
+}

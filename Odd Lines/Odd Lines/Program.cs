0b2d6082a03237893da94958689f1913fa322656
ValkovStoil <valkovstoil@abv.bg>
2020-03-29 18:46:21 +0300

    Odd Lines

---
+++ Odd Lines/Odd Lines/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Odd_Lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = 0;
+            using (var reader = new StreamReader("Lines.txt"))
+            {
+                using (var output = new StreamWriter("Output.txt"))
+                {
+
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+
+                        if (count % 2 == 1)
+                        {
+                            output.WriteLine(line, false);
+                        }
+                        count++;
+                    }
+                }
+            }
+        }
+    }
+}

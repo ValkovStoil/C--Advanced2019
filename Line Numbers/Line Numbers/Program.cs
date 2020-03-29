d89e67133cd2c2fefef686ced37c541cc15a65aa
ValkovStoil <valkovstoil@abv.bg>
2020-03-29 18:58:59 +0300

    Line Numbers

---
+++ Line Numbers/Line Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Line_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var counter = 1;
+            using (var reader = new StreamReader("Line Numbers.txt"))
+            {
+                using (var writer = new StreamWriter("Output.txt"))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+
+                        writer.WriteLine($"{counter}. {line}", false);
+                        //Console.WriteLine($"{counter}. {line}");
+
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+}

cc3d50981da4f440044f3b819c8db09b319c138b
ValkovStoil <valkovstoil@abv.bg>
2020-03-30 17:41:52 +0300

    Folder Size

---
+++ Folder Size/Folder Size/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Folder_Size
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var allFiles = Directory.GetFiles("../../../TestFolder").ToArray();
+
+            var sum = 0.0;
+            foreach (var file in allFiles)
+            {
+                var fileInfo = new FileInfo(file);
+                sum += fileInfo.Length;
+            }
+
+            using (var sr = new StreamWriter("output.txt",false))
+            {
+                sr.Write(sum / 1024 / 1024);
+            }
+
+        }
+    }
+}

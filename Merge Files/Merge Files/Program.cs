87dcc5e72d7287ae01161822914b844775d5a5aa
ValkovStoil <valkovstoil@abv.bg>
2020-03-30 16:53:26 +0300

    Merge Files

---
+++ Merge Files/Merge Files/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Merge_Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var output = File.ReadAllLines("input1.txt").ToList();
+            output.AddRange(File.ReadAllLines("input2.txt"));
+            output.Sort();
+            File.WriteAllLines("output.txt", output);
+            
+        }
+    }
+}

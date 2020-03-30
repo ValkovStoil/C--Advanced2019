65c5e25e9f4242dd4980b7924bd801b088e43579
ValkovStoil <valkovstoil@abv.bg>
2020-03-30 17:21:42 +0300

    Slice a File

---
+++ Slice a File/Slice a File/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Slice_a_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = 5;
+
+            var totalSize = new FileInfo("input.txt").Length;
+            var fileSize = (int)Math.Ceiling(totalSize / 5.0);
+
+            using (var sr = new FileStream("input.txt", FileMode.Open))
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    var buffer = new byte[fileSize];
+                    var read = sr.Read(buffer, 0, fileSize);
+
+                    using (var wr = new FileStream($"Part-{i}.txt", FileMode.OpenOrCreate, FileAccess.Write))
+                    {
+                        wr.Write(buffer, 0, read);
+                    }
+                }
+            }
+        }
+    }
+}

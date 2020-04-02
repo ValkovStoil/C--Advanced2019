02111b7050c6ae11ed1f4db7de62b88a80975a14
ValkovStoil <valkovstoil@abv.bg>
2020-04-02 17:50:34 +0300

    Zip and Extract

---
+++ Zip and Extract/Zip and Extract/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace Zip_and_Extract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var startPath = "../../../copyMe";
+            var zipPath = "../../../ziped.zip";
+            var extractPath = Environment.CurrentDirectory;
+
+            ZipFile.CreateFromDirectory(startPath, zipPath);
+
+            ZipFile.ExtractToDirectory(zipPath, extractPath);
+
+        }
+    }
+}

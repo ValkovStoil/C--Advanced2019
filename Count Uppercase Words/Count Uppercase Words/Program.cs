11afbcc17d8dd322027c5c0fdd267ccc6bdd3cc5
ValkovStoil <valkovstoil@abv.bg>
2020-04-03 16:37:04 +0300

    Count Uppercase Words

---
+++ Count Uppercase Words/Count Uppercase Words/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace Count_Uppercase_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            Func<string, bool> predicate = StartWithUpper;
+
+            foreach (var word in words)
+            {
+                if (predicate(word))
+                {
+                    Console.WriteLine(word);
+                }
+            }
+        }
+
+        private static bool StartWithUpper(string word)
+        {
+            if(word[0] >= 65 && word[0] <= 90)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}

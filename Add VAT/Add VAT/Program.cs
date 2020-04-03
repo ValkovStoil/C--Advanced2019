425252c6d7c6db2a3f58c93554113f05b7e626b6
ValkovStoil <valkovstoil@abv.bg>
2020-04-03 17:17:27 +0300

    Add VAT

---
+++ Add VAT/Add VAT/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Add_VAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var prices = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .Select(x => x = (x + x * 0.2))
+                .ToArray();
+
+
+            foreach (var price in prices)
+            {
+                Console.WriteLine($"{price:F}");
+            }
+
+        }
+    }
+}

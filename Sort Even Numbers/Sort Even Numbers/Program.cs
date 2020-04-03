6b887cb35186ac2680cddc1361c3e8a5399ae96d
ValkovStoil <valkovstoil@abv.bg>
2020-04-03 15:49:23 +0300

    Sort Even Numbers

---
+++ Sort Even Numbers/Sort Even Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Sort_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            numbers = numbers.Where(x => x % 2 == 0)
+                .ToList();
+
+            numbers.Sort();
+
+            Console.WriteLine(string.Join(", ",numbers));
+        }
+    }
+}

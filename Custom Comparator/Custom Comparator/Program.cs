c9cd88aeb17da82df623732d5e2a2d5934914441
ValkovStoil <valkovstoil@abv.bg>
2020-04-05 17:14:15 +0300

    Custom Comparator

---
+++ Custom Comparator/Custom Comparator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Custom_Comparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .OrderBy(x => x)
+                .ToArray();
+
+
+            Array.Sort(numbers,
+                (x, y) =>
+                {
+                    int compX = Math.Abs(x % 2);
+                    int compY = Math.Abs(y % 2);
+                    return compX.CompareTo(compY);
+                });
+
+            Console.WriteLine(string.Join(" ",numbers));
+        }
+    }
+}

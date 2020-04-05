654dbe7b4829f50d0c328b92bf76a70628a49c3c
ValkovStoil <valkovstoil@abv.bg>
2020-04-05 15:43:24 +0300

    Knights of Honor

---
+++ Knights of Honor/Knights of Honor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Knights_of_Honor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            var print = new Action<string[]>(Print);
+
+            print(names);
+        }
+
+        private static void Print(string[] names)
+        {
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Sir {name}");
+            }
+        }
+    }
+}

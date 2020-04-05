356788fbf7c2c15b8da3b9366e4c381ab2a85f21
ValkovStoil <valkovstoil@abv.bg>
2020-04-05 16:31:09 +0300

    Custom Min Function

---
+++ Custom Min Function/Custom Min Function/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Func<int[], int> smallest = new Func<int[], int>(SmallestNumber);
+
+            Console.WriteLine(smallest(numbers));
+        }
+
+        public static int SmallestNumber(int[] numbers)
+        {
+            var smallest = int.MaxValue;
+
+            foreach (var number in numbers)
+            {
+                if(number < smallest)
+                {
+                    smallest = number;
+                }
+            }
+
+            return smallest;
+        }
+    }
+}

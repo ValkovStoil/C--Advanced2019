7fbd0a22aa7bf61beb426da8ec73a7ae03085c45
ValkovStoil <valkovstoil@abv.bg>
2020-04-05 15:36:42 +0300

    Action Point

---
+++ Action Point/Action Point/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Action_Point
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            var print = new Action<string[]>(Print);
+
+            print(names);
+        }
+
+        private static void Print(string[] names)
+        {
+            Console.WriteLine(string.Join(Environment.NewLine,names));
+        }
+    }
+}

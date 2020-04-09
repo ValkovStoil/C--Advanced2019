930028c56fc1086e0fc428663b1e9524b2e3f3b3
ValkovStoil <valkovstoil@abv.bg>
2020-04-09 17:28:56 +0300

    Car

---
+++ Car/Car/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var car = new Car();
+
+            car.Make = "VM";
+            car.Model = "MK3";
+            car.Year = 1992;
+
+            Console.WriteLine($"Make {car.Make}\nModel: {car.Model}\nYear: {car.Year}");
+        }
+    }
+}

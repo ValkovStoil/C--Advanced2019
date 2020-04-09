111d7f77d10319b775dd4ca0416ea0019571111d
ValkovStoil <valkovstoil@abv.bg>
2020-04-09 19:12:33 +0300

    Car Constructors

---
+++ Car Constructors/Car Constructors/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Car_Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var make = Console.ReadLine();
+            var model = Console.ReadLine();
+            int year = int.Parse(Console.ReadLine());
+            double fuelQuantity = double.Parse(Console.ReadLine());
+            double feulConsumntion = double.Parse(Console.ReadLine());
+
+            var firstCar = new Car();
+            var secondCar = new Car(make, model, year);
+            var thirdCard = new Car(make, model, year, fuelQuantity, feulConsumntion);
+
+            Console.WriteLine(firstCar.WhoAmI());
+            Console.WriteLine(secondCar.WhoAmI());
+            Console.WriteLine(thirdCard.WhoAmI());
+        }
+    }
+}

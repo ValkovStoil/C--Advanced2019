cc4a46bb58159dd2a4b0790e0f1dac99b4810135
ValkovStoil <valkovstoil@abv.bg>
2020-04-10 16:18:52 +0300

    Opinion Poll

---
+++ Opinion Poll/DefiningClasses/StartUp .cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var people = new List<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var person = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                var name = person[0];
+                var age = int.Parse(person[1]);
+
+                people.Add(new Person(name, age));
+
+            }
+
+            people = people
+                .Where(a => a.Age > 30)
+                .OrderBy(n => n.Name)
+                .ToList();
+
+            foreach (var person in people)
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+        }
+    }
+}

de396fcc37598c9a31d99c6039ec72e46ae4b53c
ValkovStoil <valkovstoil@abv.bg>
2020-04-10 15:44:10 +0300

    Creating Constructors

---
+++ Creating Constructors/DefiningClasses/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name = "No name";
+        private int age = 1;
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person()
+        {
+            this.Name = this.name;
+            this.Age = this.age;
+        }
+
+        public Person(int age)
+        {
+            this.Name = this.name;
+            this.Age = age;
+        }
+        public Person(string name, int age) : this(age)
+        {
+            this.Name = name;
+        }
+
+    }
+}
---
+++ Creating Constructors/DefiningClasses/StartUp .cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            //Console.WriteLine($"Name: {person.Name}, Age: {person.Age}");
+            var person2 = new Person(20);
+            //Console.WriteLine($"Name: {person2.Name}, Age: {person2.Age}");
+            var person3 = new Person("Pesho", 15);
+            //Console.WriteLine($"Name: {person3.Name}, Age: {person3.Age}");
+
+
+        }
+    }
+}

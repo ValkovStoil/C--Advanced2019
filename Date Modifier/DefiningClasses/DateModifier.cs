b8ada4101bf517cae6bc17ae82ddbeac8fd4ecd2
ValkovStoil <valkovstoil@abv.bg>
2020-04-10 17:22:34 +0300

    Date Modifier

---
+++ Date Modifier/DefiningClasses/DateModifier.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    class DateModifier
+    {
+        private string dateOne;
+        private string dateTwo;
+
+        public string DateOne { get; set; }
+        public string DateTwo { get; set; }
+
+        public DateModifier()
+        {
+
+        }
+
+        public DateModifier(string dateOne,string dateTwo)
+        {
+            this.DateOne = dateOne;
+            this.DateTwo = dateTwo;
+        }
+
+        public double CalculateDayDifference()
+        {
+            var dateO = DateTime.Parse(this.DateOne);
+            var dateT = DateTime.Parse(this.DateTwo);
+
+            return Math.Abs((dateO - dateT).TotalDays);
+        }
+    }
+}
---
+++ Date Modifier/DefiningClasses/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var dateOne = Console.ReadLine();
+            var dateTwo = Console.ReadLine();
+
+            var dateModifire = new DateModifier(dateOne, dateTwo);
+
+            var differcen = dateModifire.CalculateDayDifference();
+
+            Console.WriteLine(differcen);
+        }
+    }
+}

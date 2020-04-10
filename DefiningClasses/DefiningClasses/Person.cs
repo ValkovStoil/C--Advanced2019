6ccba70ecd86a6fba2e3213a7c90b3e8c5b00505
ValkovStoil <valkovstoil@abv.bg>
2020-04-10 15:25:45 +0300

    Defining Classes

---
+++ DefiningClasses/DefiningClasses/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person()
+        {
+
+        }
+
+        public Person(string name,int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+    }
+}
---
+++ DefiningClasses/DefiningClasses/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DefiningClasses
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var persons = new List<Person>();
+
+            persons.Add(new Person("Pesho", 20));
+            var person = new Person();
+            person.Name = "Gosho";
+            person.Age = 18;
+            persons.Add(person);
+            persons.Add(new Person("Stamat", 43));
+        }
+    }
+}

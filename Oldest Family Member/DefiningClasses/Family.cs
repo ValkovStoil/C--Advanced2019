0059adea73ede9560ff0d96e46675014ebdd0c83
ValkovStoil <valkovstoil@abv.bg>
2020-04-10 16:05:00 +0300

    Oldest Family Member

---
+++ Oldest Family Member/DefiningClasses/Family.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        List<Person> familyMembers = new List<Person>();
+
+
+        public void AddMember(Person member)
+        {
+            familyMembers.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            return familyMembers
+                .OrderByDescending(x => x.Age)
+                .FirstOrDefault();
+        }
+    }
+}
---
+++ Oldest Family Member/DefiningClasses/StartUp .cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DefiningClasses
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var family = new Family();
+
+            for (int i = 0; i < n; i++)
+            {
+                var people = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                var name = people[0];
+                var age = int.Parse(people[1]);
+                var person = new Person(name, age);
+
+                family.AddMember(person);
+            }
+
+            var oldestMember = family.GetOldestMember();
+
+            Console.WriteLine($"{oldestMember.Name} {oldestMember.Age}");
+        }
+    }
+}

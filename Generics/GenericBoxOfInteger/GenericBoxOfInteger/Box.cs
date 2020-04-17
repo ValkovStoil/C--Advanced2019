c42f1f1330acf0d6773d5e9282f85462f2ba8204
ValkovStoil <valkovstoil@abv.bg>
2020-04-17 14:55:05 +0300

    Generic Box of Integer

---
+++ Generics/GenericBoxOfInteger/GenericBoxOfInteger/Box.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericBoxOfInteger
+{
+    public class Box<T>
+    {
+        public Box(T elemnet)
+        {
+            this.Text = elemnet;
+        }
+
+        public T Text { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{this.Text.GetType()}: {this.Text}";
+        }
+    }
+}
---
+++ Generics/GenericBoxOfInteger/GenericBoxOfInteger/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericBoxOfInteger
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var inputData = new List<Box<int>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var stringInput = int.Parse(Console.ReadLine());
+
+                inputData.Add(new Box<int>(stringInput));
+            }
+
+            foreach (var text in inputData)
+            {
+                Console.WriteLine(text.ToString());
+            }
+        }
+    }
+}

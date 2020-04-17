ad83b01412225ce8317d83592b8f4d18f4985750
ValkovStoil <valkovstoil@abv.bg>
2020-04-17 14:54:42 +0300

    Generic Box of String

---
+++ Generics/GenericBoxOfString/GenericBoxOfString/Box.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericBoxOfString
+{
+    public class Box<T>
+    {
+        public Box(T elemnet)
+        {
+            this.Text = elemnet;
+        }
+
+        public T Text { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{this.Text.GetType()}: {this.Text}";
+        }
+    }
+}
---
+++ Generics/GenericBoxOfString/GenericBoxOfString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericBoxOfString
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var inputData = new List<Box<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var stringInput = Console.ReadLine();
+
+                inputData.Add(new Box<string>(stringInput));
+            }
+
+            foreach (var text in inputData)
+            {
+                Console.WriteLine(text.ToString());
+            }
+        }
+    }
+}

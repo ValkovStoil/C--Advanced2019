dda087d4adfd73169f7ff66f2394f9222aa738b6
ValkovStoil <valkovstoil@abv.bg>
2020-04-17 16:47:16 +0300

    Generic Count Method Strings

---
+++ Generics/GenericCountMethodStrings/GenericCountMethodStrings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GenericCountMethodStrings
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var counter = int.Parse(Console.ReadLine());
+            var elements = new Box<string>();
+
+            for (int i = 0; i < counter; i++)
+            {
+                var inputData = Console.ReadLine();
+
+                elements.Value.Add(inputData);
+            }
+
+            var comparisonValue = Console.ReadLine();
+
+            var result = elements.Compare(comparisonValue);
+
+            Console.WriteLine(result);
+        }
+    }
+}

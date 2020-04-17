3f71d0f673f02f11ffee5a0143d953b65ffd3751
ValkovStoil <valkovstoil@abv.bg>
2020-04-17 16:48:29 +0300

    Generic Count Method Double

---
+++ Generics/GenericCountMethodDoubles/GenericCountMethodDoubles/Box.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericCountMethodDoubles
+{
+    public class Box<T>
+        where T: IComparable
+    {
+        public Box()
+        {
+            this.Value = new List<T>();
+        }
+
+        public List<T> Value { get; set; }
+
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            foreach (var item in this.Value)
+            {
+                sb.AppendLine($"{item.GetType()}: {item}");
+            }
+
+            var result = sb.ToString().TrimEnd();
+
+            return result;
+        }
+
+        public void SwapElements(int firstIndex, int secondIndex)
+        {
+            var tempElement = this.Value[firstIndex];
+            this.Value[firstIndex] = this.Value[secondIndex];
+            this.Value[secondIndex] = tempElement;
+        }
+
+        public int Compare(T element)
+        {
+            var count = 0;
+            foreach (var item in this.Value)
+            {
+                if(item.CompareTo(element) == 1)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Generics/GenericCountMethodDoubles/GenericCountMethodDoubles/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GenericCountMethodDoubles
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var counter = int.Parse(Console.ReadLine());
+            var elements = new Box<double>();
+
+            for (int i = 0; i < counter; i++)
+            {
+                var inputData = double.Parse(Console.ReadLine());
+
+                elements.Value.Add(inputData);
+            }
+
+            var comparisonValue = double.Parse(Console.ReadLine());
+
+            var result = elements.Compare(comparisonValue);
+
+            Console.WriteLine(result);
+        }
+    }
+}

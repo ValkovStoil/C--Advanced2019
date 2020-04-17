a14811e1adca1f1b10caedd585871615a86353ce
ValkovStoil <valkovstoil@abv.bg>
2020-04-17 17:55:13 +0300

    Tuple TODO fix 75/100 -> Run Time Error

---
+++ Generics/Tuple/Tuple/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstLine = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            var firstName = firstLine[0];
+            var secondName = firstLine[1];
+            var addres = firstLine[2];
+            var person = $"{firstName} {secondName}";
+
+            var firstTp = new TupleClass<string,string>(person, addres);
+
+            var secondLine = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            var name = secondLine[0];
+            var litersOfBeer = int.Parse(secondLine[1]);
+
+            var secondTp = new TupleClass<string, int>(name, litersOfBeer);
+
+            var thirdLine = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            var intInput = int.Parse(thirdLine[0]);
+            var doubleInput = double.Parse(thirdLine[1]);
+
+            var thirdTp = new TupleClass<int, double>(intInput, doubleInput);
+
+            Console.WriteLine(firstTp.ToString());
+            Console.WriteLine(secondTp.ToString());
+            Console.WriteLine(thirdTp.ToString());
+        }
+    }
+}
---
+++ Generics/Tuple/Tuple/TupleClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tuple
+{
+    public class TupleClass<T, T1>
+    {
+        public T Item1 { get; set; }
+
+        public T1 Item2 { get; set; }
+
+        public TupleClass(T item1, T1 item2)
+        {
+            this.Item1 = item1;
+            this.Item2 = item2;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Item1} -> {this.Item2}";
+        }
+    }
+}

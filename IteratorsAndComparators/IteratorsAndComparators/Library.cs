4e73c1ac16e2c0c2b32b629dbbf4aa207dc078f6
ValkovStoil <valkovstoil@abv.bg>
2020-04-19 16:06:41 +0300

    Library

---
+++ IteratorsAndComparators/IteratorsAndComparators/Library.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library: IEnumerable<Book>
+    {
+        public Library(params Book[] books)
+        {
+            this.Books = new List<Book>(books);
+        }
+
+        public List<Book> Books { get; private set; }
+
+        public void AddBook(Book book)
+        {
+            this.Books.Add(book);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryEnumerator(this.Books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+
+    public class LibraryEnumerator : IEnumerator<Book>
+    {
+        private int currentIndex = -1;
+
+        private readonly List<Book> books;
+
+        public LibraryEnumerator(List<Book> books)
+        {
+            this.books = books;
+        }
+
+        public Book Current => this.books[currentIndex];
+
+        object IEnumerator.Current => this.Current;
+
+        public void Dispose()
+        {
+
+        }
+
+        public bool MoveNext()
+        {
+            currentIndex++;
+            if(this.currentIndex >= this.books.Count)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void Reset()
+        {
+            currentIndex = -1;
+        }
+    }
+}
---
+++ IteratorsAndComparators/IteratorsAndComparators/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IteratorsAndComparators
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            //var library = new Library();
+
+            //library.AddBook(new Book("1984", 1980, new List<string> { "Jorge Orwell" }));
+            //library.AddBook(new Book("The Godfather", 1981, new List<string> { "Mario Puzo" }));
+            //library.AddBook(new Book("The lord of the rings", 1954, new List<string> { "J. Tolkin" }));
+
+
+            //foreach (var book in library)
+            //{
+            //    Console.WriteLine($"{string.Join(",",book.Authors)} - {book.Title} - ({book.Year})");
+            //}
+
+
+            Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
+            Book bookTwo = new Book("The Documents in the Case", 2002, "Dorothy Sayers", "Robert Eustace");
+            Book bookThree = new Book("The Documents in the Case", 1930);
+
+            Library libraryOne = new Library();
+            Library libraryTwo = new Library(bookOne, bookTwo, bookThree);
+
+        }
+    }
+}

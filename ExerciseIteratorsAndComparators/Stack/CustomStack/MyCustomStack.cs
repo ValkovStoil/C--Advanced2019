55ad026508e7c84d8facb9e7b36dd1fe7ef4d736
ValkovStoil <valkovstoil@abv.bg>
2020-04-21 16:19:48 +0300

    Stack //My Custom Stack

---
+++ ExerciseIteratorsAndComparators/Stack/CustomStack/MyCustomStack.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CustomStack
+{
+    public class MyCustomStack<T> : IEnumerable<T>
+    {
+        public MyCustomStack()
+        {
+            this.myStack = new List<T>();
+        }
+
+        public List<T> myStack { get; set; }
+
+        public void Push(T element)
+        {
+            this.myStack.Add(element);
+        }
+
+        public T Pop()
+        {
+            T element;
+
+            element = this.myStack.Last();
+            this.myStack.RemoveAt(this.myStack.Count - 1);
+
+            return element;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = this.myStack.Count -1; i >=0; i--)
+            {
+                yield return this.myStack[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+    }
+}
---
+++ ExerciseIteratorsAndComparators/Stack/CustomStack/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomStack
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var myCustomStack = new MyCustomStack<int>();
+
+            var inputCommands = Console.ReadLine()
+                .Split(" ,".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            while (inputCommands[0] != "END")
+            {
+                var command = inputCommands[0];
+                var numbers = inputCommands
+                    .Skip(1)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                if(command == "Push")
+                {
+                    foreach (var number in numbers)
+                    {
+                        myCustomStack.Push(number);
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        myCustomStack.Pop();
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("No elements");
+                    }
+                }
+
+                inputCommands = Console.ReadLine()
+                .Split(" ,".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                foreach (var elemet in myCustomStack)
+                {
+                    Console.WriteLine(elemet);
+                }
+            }
+        }
+    }
+}

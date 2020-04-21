cda9150914899f339bb5819087ad6148bd85a062
ValkovStoil <valkovstoil@abv.bg>
2020-04-21 17:05:11 +0300

    Froggy

---
+++ ExerciseIteratorsAndComparators/Froggy/Froggy/Lake.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Froggy
+{
+    public class Lake<T> : IEnumerable<T>
+    {
+        public Lake(List<T> elements)
+        {
+            this.MyLake = new List<T>(elements);
+        }
+
+        public List<T> MyLake { get; set; }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < this.MyLake.Count; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    yield return this.MyLake[i];
+                }
+            }
+
+            for (int i = this.MyLake.Count - 1; i >= 0; i--)
+            {
+                if (i % 2 != 0)
+                {
+                    yield return this.MyLake[i];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+    }
+}
---
+++ ExerciseIteratorsAndComparators/Froggy/Froggy/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Froggy
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var stones = Console.ReadLine()
+                .Split(",".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            var lake = new Lake<int>(stones);
+
+            Console.WriteLine(string.Join(", ",lake));
+        }
+    }
+}

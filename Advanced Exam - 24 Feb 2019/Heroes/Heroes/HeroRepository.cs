2a938387e0dede342be68cfa48d8e5f1c7a22963
ValkovStoil <valkovstoil@abv.bg>
2020-04-24 18:22:54 +0300

    Heroes

---
+++ Advanced Exam - 24 Feb 2019/Heroes/Heroes/HeroRepository.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Heroes
+{
+    class HeroRepository : IEnumerable<Hero>
+    {
+        List<Hero> dataCollection;
+
+        public HeroRepository()
+        {
+            dataCollection = new List<Hero>();
+        }
+
+        public void Add(Hero hero)
+        {
+            this.dataCollection.Add(hero);
+        }
+
+        public int Count => this.dataCollection.Count;
+
+        public void Remove(string name)
+        {
+            this.dataCollection = this.dataCollection
+                .Where(x => x.Name != name)
+                .ToList();
+        }
+
+         public Hero GetHeroWithHighestStrength()
+        {
+            var hero = this.dataCollection
+                .OrderByDescending(x => x.Item.Strength)
+                .FirstOrDefault();
+
+            return hero;
+        }
+        
+        public Hero GetHeroWithHighestAbility()
+        {
+
+            var hero = this.dataCollection
+                .OrderByDescending(x => x.Item.Ability)
+                .FirstOrDefault();
+
+            return hero;
+        }
+        
+        public Hero GetHeroWithHighestIntelligence()
+        {
+
+            var hero = this.dataCollection
+                .OrderByDescending(x => x.Item.Intelligence)
+                .FirstOrDefault();
+
+            return hero;
+        }
+
+        public IEnumerator<Hero> GetEnumerator()
+        {
+            foreach (var hero in this.dataCollection)
+            {
+                yield return hero;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            foreach (var hero in this.dataCollection)
+            {
+                sb.AppendLine(hero.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Advanced Exam - 24 Feb 2019/Heroes/Heroes/Item.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heroes
+{
+    class Item
+    {
+        public Item(int strength, int ability, int inteligence)
+        {
+           this.Strength = strength;
+           this.Ability = ability;
+           this.Intelligence = inteligence;
+        }
+
+        public int Strength { get; set; }
+
+        public int Ability { get; set; }
+
+        public int Intelligence { get; set; }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine("Item:");
+            sb.AppendLine($"   * Strength: {this.Strength}");
+            sb.AppendLine($"   * Ability: {this.Ability}");
+            sb.AppendLine($"   * Intelligence: {this.Intelligence}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}

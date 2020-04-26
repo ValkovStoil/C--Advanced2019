bb239ddc06fafe4eb4245d8cb2a7d77bfce59332
ValkovStoil <valkovstoil@abv.bg>
2020-04-26 17:58:22 +0300

    Rabbits //TODO

---
+++ Advanced Exam - 26 October 2019/Rabbits/Rabbits/Cage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rabbits
+{
+    class Cage
+    {
+        private List<Rabbit> data;
+
+        public Cage(string name, int cpacity)
+        {
+            this.Name = name;
+            this.Cpacity = cpacity;
+            this.data = new List<Rabbit>();
+        }
+
+        public string Name { get; set; }
+
+        public int Cpacity { get; set; }
+
+        public int Count => this.data.Count;
+
+        public void Add(Rabbit rabbit)
+        {
+            if(this.Count < this.Cpacity)
+            {
+                this.data.Add(rabbit);
+            }
+        }
+        //TODO
+        public bool RemoveRabbit(string name)
+        {
+            return true;
+        }
+    }
+}
